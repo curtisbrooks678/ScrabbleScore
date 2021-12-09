edc356f7a116284bcf25729085e3d3f0b1023fb5
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-09 11:55:56 -0800

    final commit
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>

--- ScrabbleScore/Models/Game.cs
+++ ScrabbleScore/Models/Game.cs
@@ -5,18 +5,20 @@ namespace ScrabbleTime.Models
 {
   public class Game
   {
-    public static Dictionary<string, int> scrabblePoints = new Dictionary<string, int>()
+    public static Dictionary<char, int> scrabblePoints = new Dictionary<char, int>()
     {
-      {"A", 1}, {"B", 3}, {"C", 3}, {"D", 2}, {"E", 1}, {"F", 4}, {"G", 2}, {"H", 4}, {"I", 1}, {"J", 8}, {"K", 5}, {"L", 1}, {"M", 3}, {"N", 1}, {"O", 1}, {"P", 3}, {"Q", 10}, {"R", 1}, {"S", 1}, {"T", 1}, {"U", 1}, {"V", 4}, {"W", 4}, {"X", 8}, {"Y", 4}, {"Z", 10}  
+      {'A', 1}, {'B', 3}, {'C', 3}, {'D', 2}, {'E', 1}, {'F', 4}, {'G', 2}, {'H', 4}, {'I', 1}, {'J', 8}, {'K', 5}, {'L', 1}, {'M', 3}, {'N', 1}, {'O', 1}, {'P', 3}, {'Q', 10}, {'R', 1}, {'S', 1}, {'T', 1}, {'U', 1}, {'V', 4}, {'W', 4}, {'X', 8}, {'Y', 4}, {'Z', 10}  
     };
     
     public static int Score(string word)
     {
-      // foreach (string letter in word)
-      // {
-        
-      // }
-      return 0;
+      int scoreCounter = 0;
+      char[] charArr = word.ToCharArray();
+      foreach (char ch in charArr) 
+      {
+        scoreCounter += scrabblePoints[char.ToUpper(ch)];
+      }
+      return scoreCounter;
     }
   }
 }
