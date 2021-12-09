293f3d25394aa258b04867148491830914f0b5bc
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-09 11:34:14 -0800

    green first test, return int for Score method
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>

--- ScrabbleScore.Tests/ModelTests/GameTests.cs
+++ ScrabbleScore.Tests/ModelTests/GameTests.cs
@@ -9,7 +9,7 @@ namespace ScrabbleTime.Tests
     [TestMethod]
     public void Score_ReturnsInt_int()
     {
-      Assert.AreEqual(typeof(int), Game.Score("Test"));
+      Assert.AreEqual(typeof(int), Game.Score("Test").GetType());
     }
   }
 }
